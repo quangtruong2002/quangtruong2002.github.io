1b48f677d0c62c82d376d8f8461ff27b03ecc0c2
quangtruong2002 <lzquangtruong123hpv@gmail.com>
2021-07-14 19:49:47 +0700

    Home Work Leospa

---
+++ WebForntend/box-model/main.js
@@ -0,0 +1,26 @@
+function run(datas){
+	var a = 12;
+	
+	var b = "javascript";
+
+	var c = true;
+
+	var d = ['Apple', 'Banana'];
+
+	var e = {type:"Fiat", model:"500", color:"white"};
+
+	var f = function() {
+		console.log("javascript");
+	};
+
+	var g;
+	g = console.log(typeof a);
+	console.log(typeof b);
+	console.log(typeof c);
+	console.log(typeof d);
+	console.log(typeof e);
+	console.log(typeof f);
+    
+	
+}
+console.log(g)
